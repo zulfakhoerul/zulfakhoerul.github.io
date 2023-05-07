b6cc8c389c755f61ca757730ad27a5202cbebcaa
zulfakhoerul <zulfakhoerul00@gmail.com>
2023-05-07 22:12:18 +0700

    Add photo - 07 Mei 2023

---
+++ js/typed.init.js
@@ -0,0 +1,7 @@
+// Typed Text
+var typed = new Typed("#typed", {
+  stringsElement: "#typed-strings",
+  typeSpeed: 120,
+  loop: true,
+  strings: ["IT Analyst", "Web Developer", "Software Quality Assurance"],
+});
