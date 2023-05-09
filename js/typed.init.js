a7b4f9114f6f93d684276ac00ca8b8cb2471033a
zulfakhoerul <zulfakhoerul00@gmail.com>
2023-05-09 11:37:06 +0700

    Change - 09 Mei 2023

--- js/typed.init.js
+++ js/typed.init.js
@@ -3,5 +3,5 @@ var typed = new Typed("#typed", {
   stringsElement: "#typed-strings",
   typeSpeed: 120,
   loop: true,
-  strings: ["IT Analyst", "Web Developer", "Software Quality Assurance"],
+  strings: ["System Analyst", "Business Analyst"],
 });
